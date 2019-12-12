796cec06853544ffca70da29079eb13e9a2be418
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-12 02:05:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   word = sentence.split(" ")
   reverse_word =  word.each do |new_word|
     new_word.reverse
-    reverse_word.join(" ")
+    
   end
-
+reverse_word.join(" ")
 end
