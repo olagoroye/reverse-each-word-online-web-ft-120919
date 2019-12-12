c4ed1610280a4a2e91d80441eb31e61dd809be5a
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-12 02:17:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,13 +7,13 @@ def reverse_each_word(sentence)
 new_word.join(" ")
 end
 
-# original_array = string.split(" ")
-#   return_array = []
-#   original_array.each do|string|
-#     return_array << string.reverse
-#   end
-#   return_array.join(" ")
+
 
 def reverse_each_word(sentence)
-  
+   word = sentence.split(" ")
+  new_word = []
+  word.collect do |sentence|
+    new_word << sentence.reverse
+  end
+new_word.join(" ")
 end
