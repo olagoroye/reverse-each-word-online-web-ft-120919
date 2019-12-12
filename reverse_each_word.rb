31c74ed85e7d1a71fb50fc87567f4e5c4c1a9f8b
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-12 01:26:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   my_statements = []
-  toppings.each do |topping|
+  sentence.each do |topping|
     my_statements << "I love #{topping} on my burgers!"
   end
   my_statements
